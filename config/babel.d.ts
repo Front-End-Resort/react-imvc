acf3a368fddfcfc2335508b08f06c870c53f8669
tqma113 <mtq1997@126.com>
2021-10-30 07:40:36 +0800

    fix(config): export default babel config (#31)
    * fix(config): export default babel config
    
    * chore: add config to publish files
    
    * fix: add build, start, config, context
    
    * fix(all): add module exports
    
    * fix(all): add module exports
    
    * ci: add 16.x, 17.x

---
+++ config/babel.d.ts
@@ -0,0 +1,3 @@
+import babel from '../dist/config/babel'
+export default babel
+export * from '../dist/onfig/babel'
--- hoc/connect.d.ts
+++ hoc/connect.d.ts
@@ -1,2 +1,3 @@
 import connect from '../dist/hoc/connect'
 export default connect
+export * from '../dist/hoc/connect'
